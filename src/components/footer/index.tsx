58ed2e1c6ca5a7ca05ff1d9f9c8a5686e0054fe2
jon0010 <jonnahuel78@gmail.com>
2024-04-06 20:42:26 -0300

    footer terminado

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { FaBriefcase, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-      <p>Este es el footer de la página.</p>
+    <footer className="bg-gray-800 text-white p-4 text-center fixed bottom-0 w-full flex justify-between items-center">
+      <div className="text-right">
+        <p className="text-xl font-semibold">Prueba técnica bradley</p>
+      </div>
+      <div className="text-left flex items-center">
+        <a href="https://jonnahuelpereyra.com.ar/" target="_blank">
+          <FaBriefcase className="mr-4 text-2xl" />
+        </a>
+        <a href="https://github.com/jon0010" target="_blank">
+          <FaGithub className="mr-4 text-2xl" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jon-nahuel-pereyra-832191257/"
+          target="_blank"
+        >
+          <FaLinkedin className="text-2xl" />
+        </a>
+      </div>
     </footer>
   );
 };
