c0af77dd0a928b5e1603496cdeb3cd05f3a49913
jon0010 <jonnahuel78@gmail.com>
2024-04-07 17:40:10 -0300

    app build to production

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FaBriefcase, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
